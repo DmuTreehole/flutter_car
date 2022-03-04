4b559873b9c6d379aa82024f8519013691b1ddc9
Chi-Kai <chikai0307@gmail.com>
2022-03-04 20:34:34 +0800

    v2.0

--- lib/main.dart
+++ lib/main.dart
@@ -86,7 +86,7 @@ class _MainPageState extends State<MainPage> {
   _fn(String buf) {
     _send(buf);
     setState(() {
-      tip = buf;
+      tip = '\n' + buf; // 调整文字位置
     });
   }
 
@@ -94,9 +94,15 @@ class _MainPageState extends State<MainPage> {
   _send(String buf) async {
     var sender = await UDP.bind(Endpoint.any());
     var dataLength = await sender.send(
-        buf.codeUnits, Endpoint.broadcast(port: Port(serport)));
+        buf.codeUnits, Endpoint.unicast(InternetAddress('192.168.1.1'),port: Port(serport)));
     log("$dataLength bytes sent.");
     sender.close();
+    /*
+    RawDatagramSocket rawDgramSocket =
+        await RawDatagramSocket.bind('127.0.0.1', 8080);
+    rawDgramSocket.send(
+        utf8.encode(buf), InternetAddress('192.168.1.1'), 7895);
+        */
   }
 
   // 移动函数布局
@@ -104,7 +110,8 @@ class _MainPageState extends State<MainPage> {
     return Column(
       children: <Widget>[
         // 向上
-        _move(() => _fn("up\n"), Icons.keyboard_arrow_up_sharp), //加入结束符 否则服务端不显示
+        _move(
+            () => _fn("up\n"), Icons.keyboard_arrow_up_sharp), //加入结束符 否则服务端不显示
         // 左右
         Container(
           margin: const EdgeInsets.symmetric(vertical: 70),
@@ -113,7 +120,7 @@ class _MainPageState extends State<MainPage> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               _move(() => _fn("left\n"), Icons.arrow_back_ios_new_sharp),
-              _move(() => _fn("right\n"), Icons.arrow_forward_ios_sharp), 
+              _move(() => _fn("right\n"), Icons.arrow_forward_ios_sharp),
             ],
           ),
         ),
